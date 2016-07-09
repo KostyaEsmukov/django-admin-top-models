8dd4fc6ebdd8871474650459f7d2ba858ca1693a
Kostya Esmukov <kostya@esmukov.ru>
2016-07-09 11:01:53 +0300

    Removed unneded middlewares for running tests

--- runtests.py
+++ runtests.py
@@ -21,12 +21,8 @@ class DjangoTestRunner(object):
     MIDDLEWARE_CLASSES = (
         'django.contrib.sessions.middleware.SessionMiddleware',
         'django.middleware.common.CommonMiddleware',
-        'django.middleware.csrf.CsrfViewMiddleware',
         'django.contrib.auth.middleware.AuthenticationMiddleware',
         'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-        'django.contrib.messages.middleware.MessageMiddleware',
-        'django.middleware.clickjacking.XFrameOptionsMiddleware',
-        'django.middleware.security.SecurityMiddleware',
     )
 
     def __init__(self, apps, **settings):
